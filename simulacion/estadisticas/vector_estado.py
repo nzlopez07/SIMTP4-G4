e067900002e8724f1af4acea91f3579a830ec872
Nicolas Lopez <nzlopez07@gmail.com>
2026-05-24 17:18:51 -0300

    Feature: propuesta para las estadisticas finales

--- simulacion/estadisticas/vector_estado.py
+++ simulacion/estadisticas/vector_estado.py
@@ -1,6 +1,7 @@
 """Estructuras para el vector de estado de la simulacion."""
 
 from collections import deque
+from datetime import datetime, time, timedelta
 
 from simulacion.objetos import PuestoAspirado, TunelLavado
 
@@ -30,8 +31,10 @@ class FilaVectorEstado:
         self.autos = deque()
 
         self.clientesPerdidos = 0
-        self.tiempoHorasExtras = 0.0
-        self.tiempoTunelBloqueado = 0.0
+        self.tiempoInicioBloqueoTunel = None
+        self.tiempoTunelBloqueado = timedelta(0)
+        self.tiempoFinSimulacion = None
+        self.tiempoHorasExtras = timedelta(0)
 
         self.tunel: TunelLavado = None
         self.puestoAspirado1: PuestoAspirado = None
@@ -40,25 +43,41 @@ class FilaVectorEstado:
     def como_dict(self):
         return {
             "iteracion": self.iteracion,
-            "hora_simulada": self.hora_simulada,
+            "hora_simulada": self._serializar_valor(self.hora_simulada),
             "evento_simulado": self.evento_simulado,
             "rnd_llegada": self.rndLlegada,
-            "tiempo_llegada": self.tiempoLlegada,
+            "tiempo_llegada": self._serializar_valor(self.tiempoLlegada),
             "accion_llegada": self.accionLlegada,
             "rnd_lavado": self.rndLavado,
-            "tiempo_lavado": self.tiempoLavado,
+            "tiempo_lavado": self._serializar_valor(self.tiempoLavado),
             "rnd_flag_aspirado": self.rndFlagAspirado,
             "flag_aspirado": self.flagAspirado,
             "rnd_aspirado_1": self.rndAspirado1,
-            "tiempo_aspirado_1": self.tiempoAspirado1,
+            "tiempo_aspirado_1": self._serializar_valor(self.tiempoAspirado1),
             "rnd_aspirado_2": self.rndAspirado2,
-            "tiempo_aspirado_2": self.tiempoAspirado2,
+            "tiempo_aspirado_2": self._serializar_valor(self.tiempoAspirado2),
             "contador_autos": self.contadorAutos,
             "cola_autos": self.colaAutos,
             "clientes_perdidos": self.clientesPerdidos,
-            "tiempo_horas_extras": self.tiempoHorasExtras,
-            "tiempo_tunel_bloqueado": self.tiempoTunelBloqueado,
+            "tiempo_inicio_bloqueo_tunel": self._serializar_valor(
+                self.tiempoInicioBloqueoTunel
+            ),
+            "tiempo_tunel_bloqueado": self._serializar_valor(self.tiempoTunelBloqueado),
+            "tiempo_fin_simulacion": self._serializar_valor(self.tiempoFinSimulacion),
+            "tiempo_horas_extras": self._serializar_valor(self.tiempoHorasExtras),
         }
+
+    def _serializar_valor(self, valor):
+        if isinstance(valor, datetime):
+            return valor.isoformat()
+
+        if isinstance(valor, time):
+            return valor.isoformat()
+
+        if isinstance(valor, timedelta):
+            return valor.total_seconds() / 60
+
+        return valor
 
 
 class VectorEstado:
