4e9130f1cdc0ca0e8e38e836f708f7899209eb39
Manolo1247 <joagomezmunoz@gmail.com>
2026-05-24 18:53:08 -0300

    correcion en _preparar_fila() para evitar que la nueva fila arrastre los rnd ya usados

--- simulacion/eventos/evento.py
+++ simulacion/eventos/evento.py
@@ -59,3 +59,9 @@ class Evento(ABC):
         fila_actual.iteracion = iteracion_anterior + 1
         fila_actual.hora_simulada = self.tiempo
         fila_actual.evento_simulado = self.nombre
+
+        fila_actual.rndLlegada = None
+        fila_actual.rndLavado = None
+        fila_actual.rndFlagAspirado = None
+        fila_actual.rndAspirado1 = None
+        fila_actual.rndAspirado2 = None
